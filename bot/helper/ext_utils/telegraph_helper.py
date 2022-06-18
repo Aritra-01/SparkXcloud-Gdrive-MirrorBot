a720678358f8f2410bee17510eab66424eed1813
Aritra <88967619+Aritra-01@users.noreply.github.com>
2022-06-18 21:26:29 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -72,10 +72,10 @@ class TelegraphHelper:
                     nxt_page += 1
             self.edit_page(
                 path = path[prev_page],
-                title = '𝐒𝐩𝐚𝐫𝐤𝐗𝐜𝐥𝐨𝐮𝐝-𝐆𝐝𝐫𝐢𝐯𝐞-𝐌𝐢𝐫𝐫𝐨𝐫𝐁𝐨𝐭 𝐒𝐞𝐚𝐫𝐜𝐡',
+                title = 'উন্মেষ Cloud ☁️',
                 content=content
             )
         return
 
 
-telegraph=TelegraphHelper('SparkXcloud-Gdrive-MirrorBot', 'https://github.com/Spark-X-Cloud/SparkXcloud-Gdrive-MirrorBot')
+telegraph=TelegraphHelper('Aritra Mirror Bot', 'https://t.me/Aritra_Advanced_2_Bot')
