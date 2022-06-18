4f3bf4d00530d838c8c014cea3731225cf697072
Aritra <88967619+Aritra-01@users.noreply.github.com>
2022-06-18 21:30:51 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -74,8 +74,7 @@ def start(update, context):
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-☠️ https://t.me/SparkXcloud        
-This bot can mirror all your links to Google Drive!
+        This bot can mirror all your links to Google Drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
